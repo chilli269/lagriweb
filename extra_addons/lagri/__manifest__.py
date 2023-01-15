90ea745ddb0b1df66ccb349c2cbf17d14aa30094
Mihnea <62653994+chilli269@users.noreply.github.com>
2023-01-15 12:50:41 +0200

    first commit

---
+++ extra_addons/lagri/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Lagri Group",
+    'author': 'Lagri Group',
+    'version': '15.0.0.1',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets': {
+        'web.assets_frontend': [
+        ],
+    },
+}
